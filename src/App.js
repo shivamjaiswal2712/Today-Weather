2c4be4916769f26c9c219642ef70710f3924a96e
shivamjaiswal127 <shivamjaiswal271299@gmail.com>
2023-10-02 20:51:44 +0530

    add main file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useRef, useState }from "react";
+
+const Main = () => {
+    const inputValue = useRef();
+    const [cityName, setCityName] = useState("nagpur");
+    const [error, setError] = useState(true);
+    const [lang, setLang] = useState(true);
+    const [myData, setMYData] = useState([]);
+    const [cityDetails, setCityDetails] = useState([]);
+    const [dataWeather, setDataWeather] = useState([]);
+    const [windData, setWindData] = useState([]);
+
+    useEffect(() => {
+        (async _=> {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&APPID=${APP_KEY}&units=metric&lang=${lang?('en'):('hi')}`);
+            const data = await response.json();
+            if(response.ok){
+                setCityDetails(data.city)
+                setMYData(data.list[0].main)
+                setDataWeather(data.list[0].weather[0])
+                setWindData(data.list[0].wind)
+                setError(true)
+            }else{
+                setError(false)
+            }
+        })();
+    }, [cityName, lang])
+
+    const onKeydownHandler = ((e) => {
+        if(e.key==='ENTER'){
+           e.preventDefault();
+           setCityName(inputValue.current.value)
+        }
+    })
+
+    const onSubmitHandler = ((e) => {
+        e.preventDefault();
+        setCityName(inputValue.current.value)
+    })
+
+    return (
+        <div className="box">
+            <div className="cityName">
+                {error?(<p>{cityDetails.name}, {cityDetails.country} <a href={`https://en.wikipedia.org/wiki/$(cityDetails.name)`} target="_ "> </a></p>): (<p className="invalid">{lang?'Invalid city Name':'Asaman city Name'}</p>)}
+                <div className="search">
+                    <input type="text" ref={inputValue} onKeyDown={onKeydownHandler} placeholder="city Name" />
+                    <img style={{cursor:'pointer'}} onClick={onSubmitHandler} src=""/>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Main
