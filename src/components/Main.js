cf71b4005296fd5e8f24f2c43a863a3d3810e7f6
shivamjaiswal127 <shivamjaiswal271299@gmail.com>
2023-10-03 18:08:11 +0530

    add changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,6 +9,7 @@ const Main = () => {
     const [cityDetails, setCityDetails] = useState([]);
     const [dataWeather, setDataWeather] = useState([]);
     const [windData, setWindData] = useState([]);
+    const APP_KEY = ''
 
     useEffect(() => {
         (async _=> {
@@ -44,7 +45,7 @@ const Main = () => {
                 {error?(<p>{cityDetails.name}, {cityDetails.country} <a href={`https://en.wikipedia.org/wiki/$(cityDetails.name)`} target="_ "> </a></p>): (<p className="invalid">{lang?'Invalid city Name':'Asaman city Name'}</p>)}
                 <div className="search">
                     <input type="text" ref={inputValue} onKeyDown={onKeydownHandler} placeholder="city Name" />
-                    <img style={{cursor:'pointer'}} onClick={onSubmitHandler} src=""/>
+                    <img style={{cursor:'pointer'}} onClick={onSubmitHandler} src="" alt=""/>
                 </div>
             </div>
             
